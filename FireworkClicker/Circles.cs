46366781f00725c92e1e2d8fa00525ec350d1f38
Daniel Koutris <dangvk3@gmail.com>
2022-05-03 00:58:51 -0500

    Add project files.

---
+++ FireworkClicker/Circles.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace FireworkClicker
+{
+    public class Circles: DrawableGameComponent
+    {
+
+        Game _game;
+
+        public Boolean collision = false;
+
+        public Vector2 _position;
+
+        //this will be a circle eventually
+        public Rectangle _rectange;
+
+        protected static ContentManager contentManager;
+
+        SpriteBatch spriteBatch;
+        Texture2D whiteRectangle;
+
+        int size = 100;
+
+        public Circles(Game game) : base(game)
+        {
+            _game = game;
+            //_position = position;
+            _rectange = new Rectangle(RandomHelper.Next(40,540), RandomHelper.Next(40,380), size, size);
+
+        }
+
+        public void updatePosition()
+        {
+            _rectange = new Rectangle(RandomHelper.Next(40, 540), RandomHelper.Next(40, 380), size, size);
+        }
+
+        public void shrink(GameTime gameTime, int score)
+        {
+            
+
+            if (_rectange.Width > 15)
+            {
+                if (gameTime.TotalGameTime.Milliseconds % 10 == 0)
+                {
+                    _rectange = new Rectangle(_rectange.X + 1, _rectange.Y + 1, _rectange.Width - 1, _rectange.Height - 1);
+                }
+            }
+            else
+            {
+                this.updatePosition();
+            }
+            
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            if (contentManager == null) contentManager = new ContentManager(Game.Services, "Content");
+            // Create a 1px square rectangle texture that will be scaled to the
+            // desired size and tinted the desired color at draw time
+            whiteRectangle = contentManager.Load<Texture2D>("target");//new Texture2D(GraphicsDevice, 1, 1);
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+            spriteBatch.Dispose();
+            // If you are creating your texture (instead of loading it with
+            // Content.Load) then you must Dispose of it
+            whiteRectangle.Dispose();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+            
+            spriteBatch.Begin();
+
+            // Option One (if you have integer size and coordinates)
+            if (collision)
+            {
+                spriteBatch.Draw(whiteRectangle, _rectange, Color.LightGreen);
+            }
+            else
+            {
+                spriteBatch.Draw(whiteRectangle, _rectange, Color.Red);
+            }
+
+            spriteBatch.End();
+        }
+    }
+}
---
+++ FireworkClicker/Game1.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using System.Collections.Generic;
+
+namespace FireworkClicker
+{
+    public class Game1 : Game, IParticleEmitter
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        MouseState _priorMouse;
+        KeyboardState _priorKeyboard;
+
+        Texture2D _mouseTexture;
+
+        SoundEffect errorSound;
+
+        SoundEffect fireworkExplosionSound;
+        FireworkParticleSystem _fireworks;
+
+        Song backgroundMusic;
+        SpriteFont purposeFont;
+
+        public Circles circle;
+
+        public int score;
+        public int highscore;
+        bool shake = false;
+
+        public Vector2 Position { get; set; }
+        public Vector2 Velocity { get; set; }
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            StarsParticleSystem stars = new StarsParticleSystem(this, new Rectangle(-10, -100, 800, 500));
+            Components.Add(stars);
+
+            _fireworks = new FireworkParticleSystem(this, 20);
+            Components.Add(_fireworks);
+
+            PixieParticleSystem pixie = new PixieParticleSystem(this, this);
+            Components.Add(pixie);
+
+            circle = new Circles(this);
+
+            Components.Add(circle);
+
+            score = 0;
+            highscore = 0;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            errorSound = this.Content.Load<SoundEffect>("error");
+            fireworkExplosionSound = this.Content.Load<SoundEffect>("firework");
+            backgroundMusic = this.Content.Load<Song>("renewal");
+            //MediaPlayer.Play(backgroundMusic);
+            purposeFont = Content.Load<SpriteFont>("purposeFont");
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _mouseTexture = this.Content.Load<Texture2D>("mouse");
+            Mouse.SetCursor(MouseCursor.FromTexture2D(_mouseTexture, _mouseTexture.Width / 2, _mouseTexture.Height / 2));
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            MouseState currentMouse = Mouse.GetState();
+            KeyboardState currentKeyboard = Keyboard.GetState();
+            Vector2 mousePosition = new Vector2(currentMouse.X, currentMouse.Y);
+            var mouserect = new Rectangle(mousePosition.ToPoint(), mousePosition.ToPoint());
+
+            circle.shrink(gameTime, score);
+
+            if (circle._rectange.Contains(new Point(currentMouse.X, currentMouse.Y)))
+            {
+                circle.collision = true;
+            }
+            else
+            {
+                circle.collision = false;
+                if (((currentMouse.LeftButton == ButtonState.Pressed && _priorMouse.LeftButton == ButtonState.Released) || (currentKeyboard.IsKeyDown(Keys.Space) && _priorKeyboard.IsKeyUp(Keys.Space))))
+                {
+                    if (score > highscore)
+                    {
+                        highscore = score;
+                    }
+                    score = 0;
+                    //add shake to score here
+                    shake = true;
+                    errorSound.Play(0.3f, 0, 0);
+                    circle.updatePosition();
+                }
+                else
+                {
+                    shake = false;
+                }
+            }
+
+
+            if (((currentMouse.LeftButton == ButtonState.Pressed && _priorMouse.LeftButton == ButtonState.Released) || (currentKeyboard.IsKeyDown(Keys.Space) && _priorKeyboard.IsKeyUp(Keys.Space))) && mouserect.Intersects(circle._rectange))
+            {
+
+                for (int i = 0; i < 5; i++)
+                {
+                    _fireworks.PlaceFirework(mousePosition);
+                }
+                circle.updatePosition();
+                fireworkExplosionSound.Play(0.3f, 0, 0);
+                score++;
+            }
+
+            Velocity = mousePosition - Position;
+            Position = mousePosition;
+
+            _priorKeyboard = currentKeyboard;
+            _priorMouse = currentMouse;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            Matrix tranformation = Matrix.CreateTranslation(RandomHelper.Next(-10,10), RandomHelper.Next(-10, 10), 0);
+
+            GraphicsDevice.Clear(Color.Black);
+
+            if (shake)
+            {
+                _spriteBatch.Begin(transformMatrix: tranformation);
+                _spriteBatch.DrawString(purposeFont, "Highcore: " + highscore.ToString() + "\nScore: " + score.ToString(), new Vector2(10, 10), Color.White);
+                _spriteBatch.End();
+            }
+            else
+            {
+                _spriteBatch.Begin();
+                _spriteBatch.DrawString(purposeFont, "Highcore: " + highscore.ToString() + "\nScore: " + score.ToString(), new Vector2(10, 10), Color.White);
+                _spriteBatch.End();
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+}
